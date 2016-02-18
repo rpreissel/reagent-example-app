a3850d5b47442b63a5efe431bc0685e664a02190
Rene Preissel <rene.preissel@etosquare.de>
2016-02-18 07:41:57 +0100

    restriction macro with precondition on paras

--- src/reagent_example_app/restrictions.clj
+++ src/reagent_example_app/restrictions.clj
@@ -1,6 +1,7 @@
 (ns reagent-example-app.restrictions)
 
 (defmacro defrestriction [name paras description & body]
+  {:pre [(vector? paras) (= (count paras) 1)]}
   `(defn ~name ~paras
        (let [checked# (and ~@paras (do ~@body))]
          {:label ~description
